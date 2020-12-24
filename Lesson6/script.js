40931554aa0ff41f646575c739ec42e191eca9b4
LastTen <romastas1996@gmail.com>
2020-12-25 00:03:56 +0200

    js test

---
+++ Lesson6/script.js
@@ -0,0 +1,53 @@
+/*function even_or_odd(number) {
+  if (number % 2 === 0){
+    return "Even"
+  }
+  else{
+    return "Odd"
+  }
+}
+even_or_odd(56)
+
+
+
+function makeNegative(num) {
+  if (num <= 0){
+    return num
+  }
+  else{
+    return -num
+  }
+}
+makeNegative(-25)
+
+function greet(name){
+  if(name === "Johnny"){
+    return "Hello, my love!"
+  }
+  else{
+    return "Hello, " + name + "!";
+  }
+}
+greet("Joni")
+
+
+
+function boolToWord( bool ){
+  if (bool){
+    return "Yes"
+  }
+  return "No"
+}
+
+
+function rentalCarCost(d) {
+  if (d > 0 && d < 3) {
+    return d * 40;
+  } else if (d >= 3 && d < 7) {
+    return d * 40 - 20;
+  } else if (d >= 7) {
+    return d * 40 - 50;
+  }
+}
+*/
+

730004aa841a83c67413147741bb6b374f7691aa
LastTen <romastas1996@gmail.com>
2021-01-02 12:06:00 +0200

    metods(.join, .map)

---
+++ Metods/script.js
@@ -0,0 +1,22 @@
+'use strict'
+/*
+//Пернтворення масиву в строку і назад в масив/// метод .join and .split
+let arrOFNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+console.log(arrOFNumbers);
+
+let strOfNumber = arrOFNumbers.join(" ")
+console.log(strOfNumber);
+
+let splitArrNumbers = strOfNumber.split(" ")
+console.log(splitArrNumbers);
+*/
+
+
+/*
+// Метод .map
+let arrOfNumbers1 = [1,2,3,4,5,6,7,8,9,10,]
+console.log(arrOfNumbers1);
+
+let numbersMap = arrOfNumbers1.map(num => num + 1)
+console.log(numbersMap);
+*/

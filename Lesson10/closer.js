665a624082a75d955aa48763d2782faecacd0908
LastTen <romastas1996@gmail.com>
2021-02-21 18:58:12 +0200

    ,

--- Lesson10/closer.js
+++ Lesson10/closer.js
@@ -1,4 +1,5 @@
 'use strict'
+/*
 function createSecretHolder(secret) {
   let _secret = secret;
 
@@ -12,3 +13,35 @@ function createSecretHolder(secret) {
       }
   }
 }
+
+
+function createSecretHolder(secret) {
+  let SaveYourSecret = function(newSecret) {
+    let yourSecret = newSecret;
+    this.getSecret = function() {
+      return yourSecret;
+    },
+    this.setSecret = function(secret) {
+      yourSecret = secret;
+    }
+  }
+  return new SaveYourSecret(secret);
+}
+console.log(createSecretHolder(23).getSecret());
+
+*/
+
+function createFunctions(n) {
+  var callbacks = [];
+
+  for (var i=0; i<n; i++) {
+    callbacks.push(function() {
+      return 1++
+    });
+  }
+  
+  return callbacks;
+}
+let callbacks = createFunctions(5)
+console.log(callbacks);
+console.log(callbacks[0]());
